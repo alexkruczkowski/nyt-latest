1f6d27dc5cd6cdb0d0bdb00d0a46634b41507b5c
Alex <Alex@Alexs-MacBook-Pro.local>
2021-06-08 22:27:11 -0400

    ccxt test

--- app/app.py
+++ app/app.py
@@ -1,1 +1,6 @@
 """ Check recent crypto trades, store coin info in df, move to database on completion """
+import ccxt
+
+# Use ccxt library to get market data from diff exchanges, start with coinbase and binance
+print(ccxt.exchanges)
+
