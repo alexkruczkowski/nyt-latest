d0f2ea98bd3b699b2f57847b5fd8a61ac6c8f914
Alex <Alex@Alexs-MacBook-Pro.local>
2021-06-16 22:41:53 -0400

    pytest fixture add + API discovery

--- app/books_etl.py
+++ app/books_etl.py
@@ -1,29 +1,38 @@
 """ Find the latest top stories, bestselling novels, and new movie reviews from the NYT """
 import requests
+import json
+import pandas as pd
+import numpy as np
 from api_info import API_KEY
 
 # NYT books base url, endpoints include book reviews and best seller lists
 BOOKS_BASE_URL = "https://api.nytimes.com/svc/books/v3/"
 
-def check_if_valid_data(books_dataframe):
-    """
-    Data validation used before proceeding to load stage.
-    Check if there is data, if the primary key is unique, and if dates match. 
-    """
-    pass
-
-def retrieve_books_data():
-    """
-    Use NYT API to find desired books data. Return JSON data.
-    """
+def connect_to_API():
     payload = {"api-key": API_KEY}
     r = requests.get(f"{BOOKS_BASE_URL}lists/overview.json", params=payload)
+    return r
+
+def retrieve_books_data(r):
+    """
+    Use NYT API to find books data including title, author, and description. Return pandas df.
+    """
     data = r.json()
-    return(data)
+    
+    # not working yet, test API formatting
+    for book in (data['results']['lists']['books']):
+        print(book['author'])
 
 def clean_books_data(json_input):
     """
     Manipulate data into desired format, return pandas DF. 
+    """
+    pass
+
+def check_if_valid_data(books_dataframe):
+    """
+    Data validation used before proceeding to load stage.
+    Check if there is data, if the primary key is unique, and if dates match. 
     """
     pass
 
@@ -35,5 +44,6 @@ def load_books_data(books_dataframe):
     pass
 
 # testing return functionality, add to tests
-data = retrieve_books_data()
-print(data)
+r = connect_to_API()
+retrieve_books_data(r)
+# print(data)
--- tests/books_tests.py
+++ tests/books_tests.py
@@ -6,11 +6,16 @@ from api_info import API_KEY
 # NYT books base url, endpoints include book reviews and best seller lists
 BOOKS_BASE_URL = "https://api.nytimes.com/svc/books/v3/"
 
-def get_bestsellers_overview_check_status_code_equals_200():
+@pytest.fixture
+def connect_to_API():
     payload = {"api-key": API_KEY}
     r = requests.get(f"{BOOKS_BASE_URL}lists/overview.json", params=payload)
-    assert r.status_code == 200
+    return r
 
-def get_bestsellers_overview_check_content_type_equals_json():
+def test_get_bestsellers_overview_check_status_code_equals_200(connect_to_API):
+    assert connect_to_API.status_code == 200
+
+def test_get_bestsellers_overview_check_content_type_equals_json():
+    # assert connect_to_API.headers["Content-Type"] == "application/json"
     pass
 
