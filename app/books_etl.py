65a072e4c2044037c5c37bed03039a57d417032f
Alex <Alex@Alexs-MacBook-Pro.local>
2021-06-20 21:57:35 -0400

    adding books load

--- app/books_etl.py
+++ app/books_etl.py
@@ -1,33 +1,73 @@
 """ Find the latest top stories, bestselling novels, and new movie reviews from the NYT """
 import requests
 import json
+from datetime import date, timedelta
 import pandas as pd
 import numpy as np
 from api_info import API_KEY
 
 # NYT books base url, endpoints include book reviews and best seller lists
 BOOKS_BASE_URL = "https://api.nytimes.com/svc/books/v3/"
+YESTERDAYS_DT = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
 
-def connect_to_API():
+def connect_to_API(api_url):
+    """
+    NYT bestselling books API connection to be re-used across functions.
+    """
     payload = {"api-key": API_KEY}
-    r = requests.get(f"{BOOKS_BASE_URL}lists/overview.json", params=payload)
+    r = requests.get(f"{BOOKS_BASE_URL}lists/{YESTERDAYS_DT}/{api_url}.json", params=payload)
     return r
 
 def retrieve_books_data(r):
     """
-    Use NYT API to find books data including title, author, and description. Return pandas df.
+    Use NYT API to find books data including title, author, ranking info, and description.
+    Convert date to datetime for later validation.  
+    Return pandas df.
     """
     data = r.json()
-    
-    # not working yet, test API formatting
-    for book in (data['results']['lists']['books']):
-        print(book['author'])
+    books = data['results']['books']
+    bestsellers_date = data['results']['bestsellers_date']
+    #create lists for books results to then insert into df
+    titles, authors, descriptions, ranks, last_week_ranks, weeks_on_list, bestseller_date, amazon_url = ([] for i in range(9))
+    #insert all the book info into the relevant lists
+    for book in books:
+        titles.append(book['title'])
+        authors.append(book['author'])
+        descriptions.append(book['description'])
+        ranks.append(book['rank'])
+        last_week_ranks.append(book['rank_last_week'])
+        weeks_on_list.append(book['weeks_on_list'])
+        bestseller_date.append(bestsellers_date)
+        amazon_url.append(book['amazon_product_url'])
 
-def clean_books_data(json_input):
+    #convert lists into pandas df
+    books_df = pd.DataFrame(np.column_stack([titles, authors, descriptions, ranks, last_week_ranks,\
+                                             weeks_on_list, bestseller_date, amazon_url]), 
+                                columns=["titles", "authors", "descriptions", "ranks", "last_week_ranks",\
+                                         "weeks_on_list","bestseller_date", "amazon_url"])
+
+    # Convert date column to datetime for use in validation
+    books_df['bestseller_date'] = pd.to_datetime(books_df['bestseller_date'])
+
+    return books_df
+
+def combine_books_data():
     """
-    Manipulate data into desired format, return pandas DF. 
+    Combine fiction and non fictions bestselling lists (updated weekly).
+    Output one pandas df with complete the top 15 books from each category.
     """
-    pass
+    fiction_url = "combined-print-and-e-book-fiction"
+    non_fiction_url = "combined-print-and-e-book-nonfiction"
+
+    r_fiction = connect_to_API(fiction_url)
+    r_non_fiction = connect_to_API(non_fiction_url)
+
+    df_fiction = retrieve_books_data(r_fiction)
+    df_non_fiction = retrieve_books_data(r_non_fiction)
+    
+    combined_df = pd.concat([df_fiction, df_non_fiction], ignore_index=True)
+    
+    return(combined_df)
 
 def check_if_valid_data(books_dataframe):
     """
@@ -43,7 +83,11 @@ def load_books_data(books_dataframe):
     """
     pass
 
-# testing return functionality, add to tests
-r = connect_to_API()
-retrieve_books_data(r)
-# print(data)
+def run_books_etl():
+    """
+    Run ETL bestseller list. 
+    """
+    pass
+
+df = combine_books_data()
+print(df)
--- tests/books_tests.py
+++ tests/books_tests.py
@@ -1,21 +1,25 @@
-""" Tests for NYT books API """
+""" 
+Tests for NYT books API 
+run using python -m pytest file_name.py
+"""
 import pytest 
 import requests
+from datetime import date, timedelta
+from app.books_etl import connect_to_API, retrieve_books_data
 from api_info import API_KEY
 
 # NYT books base url, endpoints include book reviews and best seller lists
 BOOKS_BASE_URL = "https://api.nytimes.com/svc/books/v3/"
+YESTERDAYS_DT = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
 
-@pytest.fixture
-def connect_to_API():
-    payload = {"api-key": API_KEY}
-    r = requests.get(f"{BOOKS_BASE_URL}lists/overview.json", params=payload)
-    return r
+def test_get_fiction_bestsellers_check_status_code_equals_200():
+    r_fiction = connect_to_API("combined-print-and-e-book-fiction")
+    assert r_fiction.status_code == 200
 
-def test_get_bestsellers_overview_check_status_code_equals_200(connect_to_API):
-    assert connect_to_API.status_code == 200
+def test_get_nonfiction_bestsellers_check_status_code_equals_200():
+    r_non_fiction = connect_to_API("combined-print-and-e-book-nonfiction")
+    assert r_non_fiction.status_code == 200
 
-def test_get_bestsellers_overview_check_content_type_equals_json():
-    # assert connect_to_API.headers["Content-Type"] == "application/json"
+def test_retrieve_book_data_check_content_equals_expected():
     pass
 
